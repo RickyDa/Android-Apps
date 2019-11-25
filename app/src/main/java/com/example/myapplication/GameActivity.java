71b18879dea31901fffbfed93fad7f98aa2a60b9
RickyDanipog <rickyd@mail.afeka.ac.il>
2019-11-25 10:40:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/GameActivity.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.ValueAnimator;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button playBtn = findViewById(R.id.playBtn);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startGame = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(startGame);
+            }
+        });
+
+        Button scoreboardBtn = findViewById(R.id.scoreboardBtn);
+        scoreboardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startGame = new Intent(getApplicationContext(), ScoreboardActivity.class);
+                startActivity(startGame);
+            }
+        });
+
+        Button exitBtn = findViewById(R.id.exitBtn);
+        exitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+    }
+}
