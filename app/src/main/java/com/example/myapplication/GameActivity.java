d2dd877fb5d50547df8854813f075f943c42a5c5
ricky <ricky.danipog4@gmail.com>
2020-01-03 17:56:20 +0200

    added two controls Sensors or touch

--- app/src/main/java/com/example/myapplication/GameActivity.java
+++ app/src/main/java/com/example/myapplication/GameActivity.java
@@ -2,18 +2,24 @@ package com.example.myapplication;
 
 import android.animation.ValueAnimator;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Random;
 
-public class GameActivity extends MyAppCompatActivity implements View.OnTouchListener {
+public class GameActivity extends MyAppCompatActivity implements View.OnTouchListener, SensorEventListener {
 
     private ViewGroup blockLayout;
 
@@ -24,21 +30,40 @@ public class GameActivity extends MyAppCompatActivity implements View.OnTouchLis
 
     private int livesLeft;
     private int bonusPoints;
-
+    private int x_player;
     private int screenHeight;
     private int screenWidth;
 
     private final int SIZE_BLOCK = 150;
-    private final long fallingDuaration =3000;
+
+    private final long fallingDuration = 3000;
     public static String TAG = "GameActivity";
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        final Bundle extra = getIntent().getExtras();
+        boolean cfg = false;
+        if (extra == null)
+            finish();
+        else {
+            cfg = extra.getBoolean(CONTROLS);
+        }
+        if (cfg)
+            findViewById(R.id.gameLayout).setOnTouchListener(this);
+        else {
+            this.sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+            assert sensorManager != null;
+            this.accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        }
 
         this.screenHeight = metrics.heightPixels;
         this.screenWidth = metrics.widthPixels;
@@ -50,7 +75,9 @@ public class GameActivity extends MyAppCompatActivity implements View.OnTouchLis
         this.hearts = new ImageView[]{findViewById(R.id.heart1), findViewById(R.id.heart2), findViewById(R.id.heart3)};
 
         this.bonusPoints = 0;
-        findViewById(R.id.gameLayout).setOnTouchListener(this);
+        this.x_player = screenWidth / 2;
+        //
+
         findViewById(R.id.startBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -83,7 +110,7 @@ public class GameActivity extends MyAppCompatActivity implements View.OnTouchLis
 
     }
 
-    private ImageView createFallingObject(int location, int id){
+    private ImageView createFallingObject(int location, int id) {
         final ImageView bonus = new ImageView(this);
 
         bonus.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
@@ -98,10 +125,10 @@ public class GameActivity extends MyAppCompatActivity implements View.OnTouchLis
     }
 
     private void createBonus(int location) {
-        final ImageView bonus = createFallingObject(location,R.drawable.shmeckels);
+        final ImageView bonus = createFallingObject(location, R.drawable.shmeckels);
         bonus.animate()
                 .translationY(screenHeight)
-                .setDuration(fallingDuaration)
+                .setDuration(fallingDuration)
                 .setUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 
                     @Override
@@ -123,10 +150,10 @@ public class GameActivity extends MyAppCompatActivity implements View.OnTouchLis
 
     private void createBlock(int location) {
 
-        final ImageView block = createFallingObject(location,R.drawable.block);
+        final ImageView block = createFallingObject(location, R.drawable.block);
         block.animate()
                 .translationY(screenHeight)
-                .setDuration(fallingDuaration)
+                .setDuration(fallingDuration)
                 .setUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 
                     @Override
@@ -135,10 +162,11 @@ public class GameActivity extends MyAppCompatActivity implements View.OnTouchLis
                             if (livesLeft >= 0) {
                                 hearts[livesLeft--].setVisibility(View.INVISIBLE);
                                 if (livesLeft < 0) {
-                                    Intent startGame = new Intent(getApplicationContext(), GameOverActivity.class);
-                                    startGame.putExtra("scoreView", scoreView.getText().toString());
+                                    Intent gameOver = new Intent(getApplicationContext(), GameOverActivity.class);
+                                    gameOver.putExtra(EXT_SCORE, scoreView.getText().toString());
+                                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
                                     finish();
-                                    startActivity(startGame);
+                                    startActivity(gameOver);
                                 }
                                 animation.end();
                             }
@@ -183,4 +211,38 @@ public class GameActivity extends MyAppCompatActivity implements View.OnTouchLis
             player.setX(x - (player.getWidth() / 2f));
         return true;
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (sensorManager != null)
+            sensorManager.registerListener(this, accelerometer,
+                    SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (sensorManager != null)
+            sensorManager.unregisterListener(this);
+        else
+            finish();
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor arg0, int arg1) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+
+            x_player -= 5 * event.values[0];
+            this.player.animate().translationX(x_player).setDuration(0).start();
+        }
+    }
+
 }
