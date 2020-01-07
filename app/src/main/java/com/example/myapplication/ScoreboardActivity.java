d12e749bb18c5169a5908a3b55bfb40a773c29f3
RickyDanipog <rickyd@mail.afeka.ac.il>
2020-01-07 13:41:58 +0200

    merge

--- app/src/main/java/com/example/myapplication/ScoreboardActivity.java
+++ app/src/main/java/com/example/myapplication/ScoreboardActivity.java
@@ -9,6 +9,12 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,12 +25,15 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ScoreboardActivity extends MyAppCompatActivity {
+public class ScoreboardActivity extends MyAppCompatActivity implements OnMapReadyCallback {
 
     private FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
     private Query mGetReference = mDatabase.getReference().child("Score").orderByChild("score");
     private String userName;
 
+    private GoogleMap mMap;
+    private float lat;
+    private float lng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +43,17 @@ public class ScoreboardActivity extends MyAppCompatActivity {
         Bundle extra = getIntent().getExtras();
         if(extra != null){
             userName = extra.getString(EXT_SCORE);
+        }
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        this.lat = -34;
+        this.lng = 151;
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
         }
 
         mGetReference.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -51,6 +71,16 @@ public class ScoreboardActivity extends MyAppCompatActivity {
             public void onCancelled(DatabaseError databaseError) {}
         });
         Log.d("Itay",list.size()+"");
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(lat, lng);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Your Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
 
